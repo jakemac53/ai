574590102673fa05b5204d723ae7f1abab4efcfb
Jake MacDonald <jakemac53@gmail.com>
2026-02-28 12:48:37 -0800

    Add Spinner component for streaming indicators

---
+++ mcp_examples/lib/ui/spinner.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:nocterm/nocterm.dart';
+
+class Spinner extends StatefulComponent {
+  final Color? color;
+  const Spinner({super.key, this.color});
+
+  static const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+  static String getFrame([int? ms]) =>
+      frames[((ms ?? DateTime.now().millisecondsSinceEpoch) ~/ 100) %
+          frames.length];
+
+  @override
+  State<Spinner> createState() => _SpinnerState();
+}
+
+class _SpinnerState extends State<Spinner> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(milliseconds: 100), (_) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Component build(BuildContext context) {
+    final theme = TuiTheme.of(context);
+    final frame = Spinner.getFrame();
+    return Text(frame, style: TextStyle(color: component.color ?? theme.primary));
+  }
+}
